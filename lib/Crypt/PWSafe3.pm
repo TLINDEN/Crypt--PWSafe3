c89d5c96451f0f4b488fa9462f845c5fcd32d6ae
git@daemon.de <git@daemon.de>
2013-07-07 09:55:53 +0200

    added lib/Crypt/PWSafe3/PasswordPolicy.pm to MANIFEST.

--- lib/Crypt/PWSafe3.pm
+++ lib/Crypt/PWSafe3.pm
@@ -22,7 +22,7 @@ use Data::Dumper;
 use Exporter ();
 use vars qw(@ISA @EXPORT);
 
-$Crypt::PWSafe3::VERSION = '1.13';
+$Crypt::PWSafe3::VERSION = '1.14';
 
 use Crypt::PWSafe3::Field;
 use Crypt::PWSafe3::HeaderField;
